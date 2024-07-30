3a59d5de853445cc5f9091eaab27ab426bc263de
nyakokitsu <140620899+nyakokitsu@users.noreply.github.com>
2024-07-30 23:49:31 +0300

    bump

--- index.py
+++ index.py
@@ -3,14 +3,14 @@
 # Inspired by this comment -- https://github.com/yt-dlp/yt-dlp/issues/10443#issuecomment-2248940967
 
 
+import requests
 import httpx
-from io import BytesIO
 import asyncio
 import platform
 import shutil
 import zipfile
-
-
+import tarfile
+import os
 
 async def main():
     """
@@ -21,17 +21,41 @@ async def main():
     latest_tag = res['tag_name']
 
     os_type = platform.system()
-    if os_type == "windows":
-        archive = BytesIO(await httpx.get(f"https://github.com/hufrea/byedpi/releases/download/{latest_tag}/byedpi-12-x86_64-w64.zip").content)
-        with zipfile.ZipFile(archive) as z:
-            with z.open('/ciadpi.exe') as zf, open('./ciadpi.exe', 'wb') as f:
+    print("nyako's byeDPI installer")
+    print("MAKE YOUTUBE GREAT AGAIN!")
+    print(f"Latest version of byeDPI: {latest_tag}")
+    print(f"Platform: {os_type}")
+    if os_type == "Windows":
+        fetch_file(f"https://github.com/hufrea/byedpi/releases/download/{latest_tag}/byedpi-12-x86_64-w64.zip", True)
+        with zipfile.ZipFile("byedpi.zip") as z:
+            with z.open('ciadpi.exe') as zf, open('./ciadpi.exe', 'wb') as f:
                 shutil.copyfileobj(zf, f)
-    elif os_type == "linux":
-        archive = BytesIO(await httpx.get(f"https://github.com/hufrea/byedpi/releases/download/{latest_tag}/byedpi-12-x86_64.tar.gz").content)
-        with zipfile.ZipFile(archive) as z:
-            with z.open('/ciadpi') as zf, open('./ciadpi', 'wb') as f:
-                shutil.copyfileobj(zf, f)
+        z.close()
+        os.remove("byedpi.zip")
+    elif os_type == "Linux":
+        fetch_file(f"https://github.com/hufrea/byedpi/releases/download/{latest_tag}/byedpi-12-x86_64.tar.gz", False)
+        tar = tarfile.open("byedpi.tar.gz")
+        tar.extract('ciadpi-x86_64')
+        tar.close()
+        os.remove("byedpi.tar.gz")
+        os.rename('ciadpi-x86_64', 'ciadpi')
+    print("File extracted!")
     
+def fetch_file(URL: str, is_win: bool):
+    try:
+        response = requests.get(URL)
+    except OSError:
+        print('No connection to the server!')
+        return None
+
+    # check if the request is succesful
+    if response.status_code == 200:
+        # Save dataset to file
+        print('File downloaded!')
+        open('byedpi.zip' if is_win else 'byedpi.tar.gz', 'wb').write(response.content)
+    else:
+        print('File request not successful!.')
+        return None
 
 if __name__ == "__main__":
     asyncio.run(main())
