10b0e96c3e60368c8c4ea6fb6c56f171669d7053
nyakokitsu <140620899+nyakokitsu@users.noreply.github.com>
2024-07-31 01:56:45 +0300

    win complete

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ import shutil
 import zipfile
 import tarfile
 import os
+import pathlib
 
 
 async def main():
@@ -23,7 +24,7 @@ async def main():
     os_type = platform.system()
     print("nyako's byeDPI installer")
     print("MAKE YOUTUBE GREAT AGAIN!\n")
-    print(f"Latest version of byeDPI: {latest_tag}")
+    print(f"Latest version of byeDPI: {latest_tag}\n")
     print(f"Platform: {os_type}")
     if os_type == "Windows":
         fetch_file(f"https://github.com/hufrea/byedpi/releases/download/{latest_tag}/byedpi-12-x86_64-w64.zip", True)
@@ -41,6 +42,30 @@ async def main():
         if os.path.exists('ciadpi-x86_64'):
             os.rename('ciadpi-x86_64', 'ciadpi')
     print("File extracted!")
+    if os_type == "Windows":
+        win_strategy()
+    elif os_type == "Linux":
+        linux_strategy()
+
+def linux_strategy():
+    pass
+
+def win_move():
+    user_dir = pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI"
+    if not user_dir.exists():
+        user_dir.mkdir(pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI")
+        shutil.copyfile("ciadpi.exe", pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI" / "ciadpi.exe")
+
+def win_strategy():
+    user_dir = pathlib.Path.home() / "AppData" / "Roaming" / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup"
+    cia_path = pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI" / "ciadpi.exe"
+    with open(user_dir / "dpi.bat", "w") as bat_file:
+        bat_file.write(f"start \"\" \"{cia_path}\"")
+    os.system("netsh winhttp set proxy socks5://127.0.0.1:10801")
+    print('Successfully added proxy! Running ciadpi...')
+    os.system(cia_path)
+
+
 
 def fetch_file(URL: str, is_win: bool):
     try:
