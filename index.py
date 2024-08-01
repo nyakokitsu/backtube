68469f3019978809ca72d0e228dc7935f13df09d
nyakokitsu <140620899+nyakokitsu@users.noreply.github.com>
2024-08-01 15:36:18 +0300

    some fixes and uninstallation progress

--- index.py
+++ index.py
@@ -11,19 +11,40 @@ import zipfile
 import tarfile
 import os
 import pathlib
+import sys
+import pyuac
 
+user_dir = pathlib.Path.home() / "AppData" / "Roaming" / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup" 
 
 async def main():
     """
     This is some cringy code plz do not read.
     """
+    
+
     # Okay, let's get actual version of byedpi
     res = requests.get("https://api.github.com/repos/hufrea/byedpi/releases/latest").json()
     latest_tag = res['tag_name']
+    
 
     os_type = platform.system()
     print("nyako's byeDPI installer")
     print("MAKE YOUTUBE GREAT AGAIN!\n")
+
+    is_installed = check_installed(os_type)
+
+    if is_installed:
+        uninstall_ask = ask("Found installation, start uninstall?")
+        if uninstall_ask:
+            if not pyuac.isUserAdmin():
+                pyuac.runAsAdmin()
+            else:
+                os.remove("%appdata%\Roaming\byeDPI\ciadpi.exe")
+                if (user_dir / "dpi.bat").exists():
+                    os.remove(user_dir / "dpi.bat")
+                print("Uninstallation complete!")
+                
+
     print(f"Latest version of byeDPI: {latest_tag}\n")
     print(f"Platform: {os_type}")
     if os_type == "Windows":
@@ -45,7 +66,10 @@ async def main():
     print("Setting some variables...")
     # Set startup.
     if os_type == "Windows":
-        win_strategy()
+        if not pyuac.isUserAdmin():
+            pyuac.runAsAdmin()
+        else:
+            win_strategy()
     elif os_type == "Linux":
         linux_strategy()
 
@@ -59,10 +83,11 @@ def win_move():
         shutil.copyfile("ciadpi.exe", pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI" / "ciadpi.exe")
 
 def win_strategy():
-    user_dir = pathlib.Path.home() / "AppData" / "Roaming" / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup"
+    need_autostart = ask("Needed to add to autostart?")
     cia_path = pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI"
-    with open(user_dir / "dpi.bat", "w") as bat_file:
-        bat_file.write(f"cd {cia_path} && ciadpi.exe -i 127.0.0.1 -p 10801 -d 1")
+    if need_autostart:
+        with open(user_dir / "dpi.bat", "w") as bat_file:
+            bat_file.write(f"cd {cia_path} && ciadpi.exe -i 127.0.0.1 -p 10801 -d 1")
     os.system("netsh winhttp set proxy socks5://127.0.0.1:10801")
     print('Successfully added proxy! Running ciadpi...')
     os.system(f"cd {cia_path} && ciadpi.exe -i 127.0.0.1 -p 10801 -d 1")
@@ -84,6 +109,43 @@ def fetch_file(URL: str, is_win: bool):
     else:
         print('File request not successful!.')
         return None
+
+
+
+
+def ask(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+def check_installed(os_type):
+    if os_type == "Windows":
+        cia_path = pathlib.Path.home() / "AppData" / "Roaming" / "byeDPI" / "ciadpi.exe"
+        if cia_path.exists():
+            return True
+        else:
+            return False
+    else:
+        print("Not found any configuration.")
+        return False
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
